fe0fae1826b82ce8f2e50e43e9d854e29d330c98
Steven Luo <sluo50@stuy.edu>
2024-11-25 14:49:41 -0500

    Child creation successful

--- main.c
+++ main.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 int main() {
+  printf("%d about to create 2 child processes.\n", getpid());
+
+  pid_t p1;
+  pid_t p2;
+  p1 = fork();
+  if (p1 < 0) {
+    perror("Fork 1 failed.");
+    exit(1);
+  }
+  else if (p1 == 0) {
+    p2 = fork();
+    if (p2 < 0) {
+      perror("Fork 2 failed.");
+      exit(1);
+    }
+  }
+
+
   return 0;
 }
