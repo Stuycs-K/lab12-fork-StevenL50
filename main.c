93609df144170989322345781a026e6a02b588ef
Steven Luo <sluo50@stuy.edu>
2024-11-26 14:33:04 -0500

    Fixed the program prioritizing first child

--- main.c
+++ main.c
@@ -6,19 +6,14 @@
 int main() {
   printf("%d about to create 2 child processes\n", getpid());
 
-  pid_t p1 = -1;
-  pid_t p2 = -1;
-  p1 = fork();
-  if (p1 < 0) {
+  pid_t p = -1;
+  int child = 0;
+  p = fork();
+  if (p < 0) {
     perror("Fork 1 failed.");
-    exit(1);
+    exit(-1);
   }
-  else if (p1 == 0) {
-    p2 = fork();
-    if (p2 < 0) {
-      perror("Fork 2 failed.");
-      exit(1);
-    }
+  else if (p == 0) {
     srand(getpid());
     int time = rand() % 5 + 1;
     printf("%d %dsec\n", getpid(), time);
@@ -26,11 +21,24 @@ int main() {
     printf("%d finished after %dsec\n", getpid(), time);
     exit(time);
   }
-  else {
-    int status = 0;
-    int pid = wait(&status);
-    int time = WEXITSTATUS(status);
-    printf("Main Process %d is done. Child %d slept for %dsec\n", getpid(), pid, time);
+
+  p = fork();
+  if (p < 0) {
+    perror("Fork 2 failed.");
+    exit(-1);
   }
+  else if (p == 0) {
+    srand(getpid());
+    int time = rand() % 5 + 1;
+    printf("%d %dsec\n", getpid(), time);
+    sleep(time);
+    printf("%d finished after %dsec\n", getpid(), time);
+    exit(time);
+  }
+  
+  int status = 0;
+  pid_t pid = wait(&status);
+  int time = WEXITSTATUS(status);
+  printf("Main Process %d is done. Child %d slept for %dsec\n", getpid(), pid, time);
   return 0;
 }
